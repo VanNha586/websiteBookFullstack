d96d1ac4e89edf6520942b2159c3d220adfeaec5
VanNha586 <82138603+VanNha586@users.noreply.github.com>
2022-05-14 16:38:19 +0700

    khoi tao project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Button } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Button color="secondary" className="p-4">
+          Click Me
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
