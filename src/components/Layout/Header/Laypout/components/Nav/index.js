3b5237a97dccb016f43ad0639491ada70cbd4607
VanNha586 <82138603+VanNha586@users.noreply.github.com>
2022-05-17 10:11:54 +0700

    nav

--- src/components/Layout/Header/Laypout/components/Nav/index.js
+++ src/components/Layout/Header/Laypout/components/Nav/index.js
@@ -5,7 +5,58 @@ import styles from "./Nav.module.scss";
 const cx = classNames.bind(styles);
 
 function Nav() {
-  return <p className={cx("nav")}>1</p>;
+  return (
+    <div className={cx("wrapper_nav")}>
+      <div className={cx("container")}>
+        <ul className={cx("nav")}>
+          <li className={cx("nav-item")}>
+            <a href="#" className={cx("nav-link")}>
+              All Categories
+              <i class="fa-solid fa-angle-down"></i>
+            </a>
+          </li>
+          <li className={cx("nav-item")}>
+            <a href="#" className={cx("nav-link")}>
+              Home
+              <i class="fa-solid fa-angle-down"></i>
+            </a>
+          </li>
+          <li className={cx("nav-item")}>
+            <a href="#" className={cx("nav-link")}>
+              Authors
+              <i class="fa-solid fa-angle-down"></i>
+            </a>
+          </li>
+          <li className={cx("nav-item")}>
+            <a href="#" className={cx("nav-link")}>
+              Best Selling
+            </a>
+          </li>
+          <li className={cx("nav-item")}>
+            <a href="#" className={cx("nav-link")}>
+              Weekly Sale
+            </a>
+          </li>
+          <li className={cx("nav-item")}>
+            <a href="#" className={cx("nav-link")}>
+              Latest News
+              <i class="fa-solid fa-angle-down"></i>
+            </a>
+          </li>
+          <li className={cx("nav-item")}>
+            <a href="#" className={cx("nav-link")}>
+              Contact
+            </a>
+          </li>
+          <li className={cx("nav-item")}>
+            <a href="#" className={cx("nav-link")}>
+              <i class="fa-solid fa-bars-staggered"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Nav;
